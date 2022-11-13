ad9d734a1c0089a89254e988827bc78a941eec69
Ewie1 <shephvo12@gmail.com>
2022-11-13 21:13:39 +0000

    Add question and answer for game level two

--- questionanswer_lev_one.py
+++ questionanswer_lev_one.py
@@ -43,43 +43,43 @@ questions = {
 
 level_two = {
     1 : {
-        "question" : "Hi, I'm Chandler. I make jokes when I'm uncomfortable",
-        "answer" : "Friends"
+        "question" : "Theodore Bagwell, Micheal Scofeild",
+        "answer" : "prison break"
     },
 
     2 : {
-        "question" : "Gosh Amy. I'm sensing a little hostility. Is it maybe because like Sheldon's work your sex life is also theoretical?",
-        "answer" : "The Big Bang Theory"        
+        "question" : "Ofwarren, Serena Joy, Ofglen",
+        "answer" : "the handmaid's tale"        
     },
 
     3 : {
-        "question" : "No need to seize the last word Lord Baelish. I'll assume it was something clever",
+        "question" : "Jon Snow, Samwell Tarly",
         "answer" : "Game of Thrones"
     },
 
     4 : {
-        "question" : "Jerry, just remeber, it,s not a lie if you believe it",
-         "answer" : "Seinfeld"
+        "question" : "Phillip Banks, Will Smith",
+         "answer" : "the fresh prince of belair"
     },
 
     5 : {
-        "question" : "You either run from things or you face them, Mr. White.",
-        "answer" : "Breaking Bad"
+        "question" : "Micheal Kelso, Laurie Forman, Steven Hyde",
+        "answer" : "that 70s show"
     },
     
     6 : {
-        "question" : "Oh my God! They killed Kenny",
-        "answer" : "South Park"
+        "question" : "Leonard Hofstadter, Bernadette Rostenkowski",
+        "answer" : "the big bang theory"
     },
 
     7 : {
-        "question" : "Winter is coming...",
-        "answer" : "Game of Thrones"
+        "question" : "Monica Geller, Joey Tribiani, Phoebe Buffay",
+        "answer" : "Friends"
     },
 
     8 : {
-        "question" : "Luke, don't give in to the hate. That leads to the dark side",
-        "answer" : "Star Wars"
+        "question" : "Don Draper, Peggy Olson",
+        "answer" : "mad men"
     }
 }
 
